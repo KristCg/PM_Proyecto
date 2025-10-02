6e912ebc3c1eea39b51aad8db57a03646097d70c
KristCg <kriscastillo515@gmail.com>
2025-10-02 13:24:27 -0600

    Correcion del archivo theme.kt y finalizacíón de Edit Profile Screen

--- app/src/main/java/com/kriscg/belek/Screens/Profile/EditProfileScreen.kt
+++ app/src/main/java/com/kriscg/belek/Screens/Profile/EditProfileScreen.kt
@@ -1,15 +1,22 @@
 package com.kriscg.belek.Screens.Profile
 
+import android.R
+import androidx.compose.animation.core.estimateAnimationDurationMillis
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -21,9 +28,11 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -61,7 +70,8 @@ fun EditProfileScreen(
                 imageVector = Icons.Default.AccountCircle,
                 contentDescription = "Perfil",
                 modifier = Modifier
-                    .size(175.dp)
+                    .size(175.dp),
+                tint = MaterialTheme.colorScheme.onPrimaryContainer
             )
             Button(
                 onClick = {})
@@ -73,23 +83,75 @@ fun EditProfileScreen(
             }
             Spacer(modifier = Modifier.height(25.dp))
 
+            InfoContent(
+                titulo = "Nombre de Usuario",
+                contenido = "Nombre_de_usuario"
+            )
+            InfoContent(
+                titulo = "Correo Electronico",
+                contenido = "CorreoUsuario@example.com"
+            )
+            InfoContent(
+                titulo = "Descripción",
+                contenido = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            )
+            InfoContent(
+                titulo = "Contraseña",
+                contenido = "Contraseñaejemplo"
+            )
+
+
         }
     }
 }
 
 @Composable
 fun InfoContent(
-    modifier: Modifier = Modifier
-){
-    Row () {
-        Text(
-            text = "Texto1"
+    modifier: Modifier = Modifier,
+    titulo: String,
+    contenido: String
+) {
+    val mostrarContenido = if (titulo.equals("Contraseña", ignoreCase = true)) {
+        "*".repeat(contenido.length.coerceAtLeast(6))
+    } else {
+        contenido
+    }
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 30.dp, vertical = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
 
+        ) {
+            Text(
+                text = titulo,
+                fontWeight = FontWeight.SemiBold,
+                style = MaterialTheme.typography.titleMedium,
+                fontSize = 17.sp
+            )
+            Box(
+                modifier = Modifier
+            ) {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Create,
+                        contentDescription = "Editar"
+                    )
+                }
+            }
+        }
+        Text(
+            text = mostrarContenido,
+            style = MaterialTheme.typography.bodyMedium,
+            fontSize = 17.sp,
         )
     }
-    Text(
-        text = "Texto 2"
-    )
 }
 
 @Preview(showBackground = true)
